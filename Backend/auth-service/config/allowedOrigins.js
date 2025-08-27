7229a602dd53ac9c2a90d00a3272b22b2ed3bb24
Thien Ilyass Tran <uih04743@contiwan.com>
2025-08-27 11:02:05 +0200

    Add additional allowed origin for CORS configuration

--- Backend/auth-service/config/allowedOrigins.js
+++ Backend/auth-service/config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     process.env.MESSAGE_SERVICE_URL,
     process.env.NOTIFICATION_SERVICE_URL,
     process.env.POST_SERVICE_URL,
-    "https://breezy-dad-6-dlsz.vercel.app"
+    "https://breezy-dad-6-dlsz.vercel.app",
+    "https://vercel.com/ilyass1309s-projects/breezy-dad-6-dlsz/5DtBoWWLsK9TGDDHyaqcJw6gfELY"
 ].filter(Boolean); // retire les valeurs undefined
 
 module.exports = allowedOrigins;
--- Backend/notification-service/config/allowedOrigins.js
+++ Backend/notification-service/config/allowedOrigins.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
     process.env.NOTIFICATION_SERVICE_URL,
     process.env.POST_SERVICE_URL,
     "https://breezy-dad-6-dlsz.vercel.app",
+    "https://vercel.com/ilyass1309s-projects/breezy-dad-6-dlsz/5DtBoWWLsK9TGDDHyaqcJw6gfELY"
 ].filter(Boolean);
 
 module.exports = allowedOrigins;

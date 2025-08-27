6d5114e3fb781c0832fe59ea80a6e32fbc6f30e9
Thien Ilyass Tran <uih04743@contiwan.com>
2025-08-27 10:48:21 +0200

    Enhance CORS logging for received and authorized origins

--- Backend/auth-service/config/corsOptions.js
+++ Backend/auth-service/config/corsOptions.js
@@ -2,10 +2,13 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log('[CORS] Origin reçu :', origin);
         if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            console.log('[CORS] Origin autorisé');
             callback(null, true);
         }
         else{
+            console.warn('[CORS] Origin refusé :', origin);
             callback(new Error('Not allowed by CORS'));
         }
     },
